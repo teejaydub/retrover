fa579aa4d366dae20a7d91b225863b2734fae16c
teejaydub <tw@timothyweber.org>
2024-12-13 09:20:53 -0500

    Added --nopulse and --windowSecs, to look for the absence of a string.

--- README.md
+++ README.md
@@ -21,37 +21,36 @@ Press Ctrl+C once to see a summary of what's been seen so far, and again to quit
 ## Command-line options
 
 ```
-usage: retrover.py [-h] [--baud [BAUD]] [--log LOGFILE]
-                   [--window WINDOWRADIUS] [--eventrun]
-                   [--delta] [--single] --regex REGEX
-                   [REGEX ...] [--ignorecase] [--utc]
+usage: retrover.py [-h] [--baud [BAUD]] [--log LOGFILE] [--window WINDOWRADIUS] [--windowSecs WINDOWSECS] [--eventrun]
+                   [--delta] [--single] [--nopulse] --regex REGEX [REGEX ...] [--ignorecase] [--utc]
+                   PORT [PORT ...]
+
+Watch serial ports for events, and log them.
 
 positional arguments:
   PORT                  a serial port to watch
 
 options:
   -h, --help            show this help message and exit
-
   --baud [BAUD]         baud rate for all serial ports
-
   --log LOGFILE         file to log events to
-
   --window WINDOWRADIUS
                         Log this many lines before and after the event.
-
+  --windowSecs WINDOWSECS
+                        Log lines for this many seconds before and after the event.
   --eventrun            An event is counted for each run of lines that has any match.
-
   --delta               An event is counted for each match that's different from the previous line.
-
   --single              An event is counted for each match.
-
+  --nopulse             An event is counted when there's been *no* match over the given window radius.
   --regex REGEX [REGEX ...]
                         A line that matches one or more regexes, anywhere, is an event.
-
   --ignorecase          whether to distinguish upper and lower case letters or not
-
   --utc                 whether to log times in UTC, vs. local time
 ```
+
+If multiple event definition arguments ("An event is counted...") are specified, only the last one is used.
+That is, there's only one way events are counted for each invocation of Retrover.
+
 ## File output
 
 The lines around events are written to `retrover.log` in the current directory, like this:
@@ -63,7 +62,7 @@ The lines around events are written to `retrover.log` in the current directory, 
 2023-08-07 14:35:47.210118  > OTA:
 2023-08-07 14:35:47.312100  > ~S=7
 ==
-== EVENT FOUND ==
+== EVENT FOUND (#1) ==
 ==
 2023-08-07 14:35:47.424675  > Up?
 2023-08-07 14:35:48.520659  > No.
@@ -79,6 +78,8 @@ A number of lines are included before and after the event equal to the `--window
 
 For two serial ports, lines from the first port listed on the command line are preceded by `< `,
 and lines from the second one are preceded by ` >`. 
+
+If `--nopulse` is specified, lines that match the pulse are preceded by an asterisk (`*`).
 
 ## Console output
 
--- retrover.py
+++ retrover.py
@@ -17,8 +17,6 @@
   Use -h or --help for more description of command-line arguments.
 
   Press Ctrl+C once to see a summary of what's been seen so far, and again to quit.
-
-  Also set the options below - directly in this script; seems simpler for occasional use.
 """
 
 import argparse
@@ -28,7 +26,7 @@ import sys
 import time
 
 from collections import deque
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import serial
 
@@ -37,6 +35,7 @@ import serial
 EVENT_MATCH = 'match'  # Simply if a regex is found in a line.
 EVENT_DELTA = 'delta'  # If a regex is found, AND the line differs from the previously-found match.
 EVENT_RUN = 'run'    # If a regex is found, AND there has not been a match in the previous args.windowRadius lines.
+EVENT_NOPULSE = 'nopulse'   # If the regex hasn't been seen for args.windowTime seconds.
 
 
 serialPortNames = []
@@ -46,6 +45,7 @@ previousMatchLine = ''
 
 
 def _now():
+    ''' Return the current time, in the configured timezone. '''
     global args
     if args.utc:
         return datetime.utcnow()
@@ -77,14 +77,25 @@ def logLine(line, timestamp=True):
     ''' Add this line to a managed log, that will be dumped to a file only around events.
       If timestamp is False, omit the usual prefix of the date and time.
     '''
-    global args
-    log.append([_now() if timestamp else "", line])
+    global args, log, waitingForLogLines, saw_pulse
+    log.append([_now() if timestamp else "", line, saw_pulse])
     print(numEvents, line)
     maybeOutput();
-    while len(log) > args.windowRadius:
+
+    # Remove lines we no longer need from the log.
+    # If this line is a pulse, remove all the previous lines.
+    # Keep the windowRadius at a minimum.
+    while len(log) > args.windowRadius and (args.mode != EVENT_NOPULSE or saw_pulse):
         log.popleft()
 
+def isOlderThanWindow(t, pad=0):
+    ''' Return True if time t is longer ago than the window seconds argument.
+        If pad is specified, extend the window by that many seconds.
+    '''
+    return t < (_now() - timedelta(seconds=args.windowSecs + pad))
+
 def maybeOutput():
+    ''' Write pending lines to the log file, and remove them from the log. '''
     global waitingForLogLines, log
     # Send output to the log file if we are still logging after an event.
     while waitingForLogLines and len(log):
@@ -101,8 +112,10 @@ def writeToFile(logEntry):
     print(line, file=args.logFile)
 
 def isEvent(line: str):
-    # Return true iff line triggers an event.
-    global args, regexes, previousMatchLine
+    ''' Return true iff line triggers an event.
+        If we're looking for no-pulse and it *is* a pulse, set the global saw_pulse.
+    '''
+    global args, regexes, previousMatchLine, last_match_time, saw_pulse
 
     # See if we have a match.
     if any([regex.search(line) for regex in regexes]):
@@ -112,19 +125,25 @@ def isEvent(line: str):
             if line != previousMatchLine:
                 previousMatchLine = line
                 return True
+        elif args.mode == EVENT_NOPULSE:
+            last_match_time = _now()
+            saw_pulse = True
+    elif args.mode == EVENT_NOPULSE and isOlderThanWindow(last_match_time):
+        return True
     return False
 
 def logEvent():
-    global args, waitingForLogLines, numEvents
+    global args, log, waitingForLogLines, numEvents
 
     isInRun = args.mode == EVENT_RUN and waitingForLogLines > 0
+    isInRun = isInRun or args.mode == EVENT_NOPULSE and len(log) == 0
 
     # Output an event separator if we're not currently showing an event.
     if not isInRun:
       writeToFile(["", ""])
 
-    # Output up to args.windowRadius lines that we already have.
-    waitingForLogLines = args.windowRadius
+    # Output all the lines that we already have saved.
+    waitingForLogLines = len(log)
     maybeOutput()
 
     # Output some stuff about the event.
@@ -139,8 +158,8 @@ def logEvent():
 def processPort(i):
     # Read and echo a line from the given port.
     # Log events if they're found.
-    # Return it; '' means there was a timeout and nothing was read.
-    global numEvents, serialPorts
+    # Return the line read; '' means there was a timeout and nothing was read.
+    global numEvents, serialPorts, saw_pulse
 
     # Read the next line.
     thePort = serialPorts[i]
@@ -152,11 +171,14 @@ def processPort(i):
 
     if line != '':
         line = line.strip()
-        logLine(headerForPort(i) + ' ' + line)
 
         # Look for events.
+        saw_pulse = False
         if (isEvent(line)):
+            logLine(headerForPort(i) + ' ' + line)
             logEvent()
+        else:
+            logLine(headerForPort(i) + ('*' if saw_pulse else ' ') + line)
 
     return line
 
@@ -165,13 +187,15 @@ def processPort(i):
 parser = argparse.ArgumentParser(description='Watch serial ports for events, and log them.')
 parser.add_argument('serialPorts', metavar='PORT', nargs='+',
                     help='a serial port to watch')
-parser.add_argument('--baud', nargs='?', default=9600,
+parser.add_argument('--baud', nargs='?', default=9600, type=int,
                     help='baud rate for all serial ports')
 parser.add_argument('--log', dest='logFile', default='retrover.log',
                     type=argparse.FileType('a', bufsize=1),
                     help='file to log events to')
-parser.add_argument('--window', dest='windowRadius', default=10,
+parser.add_argument('--window', dest='windowRadius', default=10, type=int,
                     help='Log this many lines before and after the event.')
+parser.add_argument('--windowSecs', dest='windowSecs', default=16, type=int,
+                    help='Log lines for this many seconds before and after the event.')
 
 parser.add_argument('--eventrun', dest='mode', action='store_const', const=EVENT_RUN, default=EVENT_RUN,
                     help="An event is counted for each run of lines that has any match.")
@@ -179,6 +203,8 @@ parser.add_argument('--delta', dest='mode', action='store_const', const=EVENT_DE
                     help="An event is counted for each match that's different from the previous line.")
 parser.add_argument('--single', dest='mode', action='store_const', const=EVENT_MATCH,
                     help='An event is counted for each match.')
+parser.add_argument('--nopulse', dest='mode', action='store_const', const=EVENT_NOPULSE,
+                    help='An event is counted when there\'s been *no* match over the given window radius.')
 
 parser.add_argument('--regex', nargs='+', required=True, action='extend',
                     help='A line that matches one or more regexes, anywhere, is an event.')
@@ -190,23 +216,33 @@ parser.add_argument('--utc', action='store_true',
 
 args = parser.parse_args()
 
+
 # Serial port setup
 for nextPort in args.serialPorts:
     print("Connecting to serial port '{}'.".format(nextPort))
     serialPortNames.append(nextPort)
     serialPorts.append(serial.Serial(nextPort, args.baud, timeout=0.1))
 
+
+# Other set up and banner.
 regexes = [re.compile(r, re.IGNORECASE if args.ignorecase else 0) for r in args.regex]
 
 print('', file=args.logFile)
 writeToFile([_now(), 'Start run.'])
 
 summary = "Searching for regexes: " + str(args.regex) + ", counting by " + args.mode
+if args.mode == EVENT_NOPULSE:
+    summary += ' in ' + str(args.windowSecs) + ' sec'
+summary += ' with window radius ' + str(args.windowRadius)
+
 writeToFile([_now(), summary])
 print(summary);
 print("Press Ctrl+C to see stats.\n----")
 
 start_time = _now()
+last_match_time = _now()
+saw_pulse = False  # on the current line.
+
 
 # Read loop
 try:
@@ -223,4 +259,6 @@ try:
 except KeyboardInterrupt:
     pass
 
+
+# Cleanup.
 writeToFile([_now(), 'Closing.'])
